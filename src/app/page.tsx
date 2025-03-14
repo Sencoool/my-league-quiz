97b145d8231cbf59d0020081fb8c590c7c1c3fa3
Sencoool <s6506021610037@email.kmutnb.ac.th>
2025-03-15 01:05:29 +0700

    Filter now works as expected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import axios from "axios"
+import { log } from "console";
 import {useEffect, useState } from "react"
 
 export default function Home() {
@@ -10,21 +11,18 @@ export default function Home() {
 
   const searchCharacter = (input: string) => {
     if (input === "") {
-      setSearch([]); // ไม่พิมพ์ ไม่แสดงผลลัพธ์
+      setSearch([]); // Not showing anything if input is empty
       return;
     }
   
-    let data = champions.filter((c: any) =>
-      c.name.toLowerCase().includes(input.toLowerCase())
+    let data = champions.filter((champions: any) => 
+      champions.name.toLowerCase().startsWith(input.toLowerCase()) // Start with make input filter more accurate
     );
+    
   
     setSearch(data);
   };
   
-
-  const changeSelectStage = (index: any, e: any) => {
-    let data = [...search];
-  };
 
   useEffect(() => {
     async function getChampions() {
@@ -54,10 +52,10 @@ export default function Home() {
         <div className="container mx-auto items-center flex flex-col border-2 border-blue-500 p-4 rounded-lg w-1/2 h-[350px]">
           <h1 className="text-3xl font-bold mb-10">League Champions Guessing</h1>
           <h1>Answer is : {randomChampion.name} </h1>
-          <input type="text" list="characters" name="search" id="search" className="bg-white text-black p-2 w-1/2 rounded-lg" placeholder="Type character name . . . ." onChange={e => searchCharacter(e.target.value)} autoComplete="off"/>
+          <input type="text" list="characters" name="search" id="search" className="bg-white text-black p-2 w-1/2 rounded-lg" placeholder="Type character name . . . ." onChange={e => searchCharacter(e.target.value)}/>
           <div className="relative w-1/2 max-h-[340px] overflow-y-scroll custom-scrollbar">
           {search.map((champion: any, index) => (
-            <div className="flex items-center gap-4 p-2 border-b border-[#ccc] hover:bg-[#dadada] duration-150 cursor-pointer" key={index} onClick={(e) => changeSelectStage(index, e)}>
+            <div className="flex items-center gap-4 p-2 border-b border-[#ccc] hover:bg-[#dadada] duration-150 cursor-pointer" key={index}>
               <img width={50} height={50} src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.id}.png`}  alt="" className="w-10 h-10 object-cover"/>
               {champion.name}
             </div>
