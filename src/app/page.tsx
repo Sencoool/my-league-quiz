680e4bcb01d980e486728089729f805f79f6ef65
Sencoool <s6506021610037@email.kmutnb.ac.th>
2025-03-15 01:28:30 +0700

    Adjust Guessing box CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,37 +9,24 @@ export default function Home() {
   const [search, setSearch] = useState([]);
   const [randomChampion, setRandomChampion] = useState<any>([]);
 
+  // Search character
   const searchCharacter = (input: string) => {
-    if (input === "") {
-      setSearch([]); // Not showing anything if input is empty
-      return;
-    }
-  
     let data = champions.filter((champions: any) => 
       champions.name.toLowerCase().startsWith(input.toLowerCase()) // Start with make input filter more accurate
     );
-    
-  
     setSearch(data);
   };
   
-
+  // Get champions
   useEffect(() => {
     async function getChampions() {
       const res = await axios.get(
         "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/en_US/champion.json"
       );
-      let championsArray: any = Object.values(res.data.data);
-  
-      // เพิ่ม property `select` เพื่อป้องกัน error
-      championsArray = championsArray.map((champion: any) => ({
-        ...champion,
-      }));
-  
+      let championsArray: any = Object.values(res.data.data); // Change object to array of champions
       setChampions(championsArray);
-      setSearch([]); // เริ่มต้นให้ไม่มีรายการแสดง
   
-      const index = Math.floor(Math.random() * championsArray.length);
+      const index = Math.floor(Math.random() * championsArray.length); // Randomize champion
       setRandomChampion(championsArray[index]);
     }
     getChampions();
@@ -48,14 +35,14 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex flex-col items-center justify-center min-h-screen py-2 container mx-auto">
-        <div className="container mx-auto items-center flex flex-col border-2 border-blue-500 p-4 rounded-lg w-1/2 h-[350px]">
+      <main className="flex flex-col items-center min-h-screen py-2 container mx-auto pt-[100px]">
+        <div className="container mx-auto items-center flex flex-col border-2 border-blue-500 p-4 rounded-lg w-1/2">
           <h1 className="text-3xl font-bold mb-10">League Champions Guessing</h1>
           <h1>Answer is : {randomChampion.name} </h1>
           <input type="text" list="characters" name="search" id="search" className="bg-white text-black p-2 w-1/2 rounded-lg" placeholder="Type character name . . . ." onChange={e => searchCharacter(e.target.value)}/>
           <div className="relative w-1/2 max-h-[340px] overflow-y-scroll custom-scrollbar">
           {search.map((champion: any, index) => (
-            <div className="flex items-center gap-4 p-2 border-b border-[#ccc] hover:bg-[#dadada] duration-150 cursor-pointer" key={index}>
+            <div className="flex items-center gap-4 p-2 border-b bg-neutral-800 border-[#ccc] hover:bg-rose-950 duration-300 cursor-pointer" key={index}>
               <img width={50} height={50} src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.id}.png`}  alt="" className="w-10 h-10 object-cover"/>
               {champion.name}
             </div>
