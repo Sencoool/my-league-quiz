17119633df1794e646124a25f99f688e04b2f675
Sencoool <s6506021610037@email.kmutnb.ac.th>
2025-03-16 02:40:40 +0700

    add answer function with click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,18 +63,28 @@ export default function Home() {
           <input type="text" list="characters" name="search" id="search" className="bg-white text-black p-2 w-1/2 rounded-lg" placeholder="Type character name . . . ." onChange={e => searchCharacter(e.target.value)} ref={inputRef}/>
           <div className="relative w-1/2 max-h-[340px] overflow-y-scroll">
           {search.map((champion: any, index) => (
-            <div className="flex items-center gap-4 p-2 border-b bg-neutral-800 border-[#ccc] hover:bg-rose-950 duration-300 cursor-pointer" key={index} onClick={e => handleClick(champion.id)}>
+            <div className="flex items-center gap-4 p-2 border-b bg-neutral-800 border-[#ccc] hover:bg-rose-950 duration-300 cursor-pointer" key={index} onClick={e => handleClick(champion)}>
               <img width={50} height={50} src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.id}.png`}  alt="" className="w-10 h-10 object-cover" />
               {champion.name}
             </div>
           ))}
           </div>
         </div>
-        <div className="flex flex-col items-center mt-10">
+        <div className="flex flex-col items-center mt-10 w-1/2">
            Already Answered
            <div>
-            {answer.map((champions, index) => (
-              <div key={index}>{champions}</div>
+            {answer.map((champions : any, index) => (
+              <div className="flex" key={index}>
+                <div className="border-2 border-indigo-500 p-2 max-w-[150px]">
+                  <img src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champions.id}.png`} alt="" className="w-15 object-cover"/>
+                </div>
+                <div className="border-2 border-indigo-500 p-2 w-[150px]">
+                  {champions.name}
+                </div>
+                <div className="border-2 border-indigo-500 p-2 w-[150px]">  
+                  {champions.partype}
+                </div>
+              </div>
             ))}
            </div>
         </div>
