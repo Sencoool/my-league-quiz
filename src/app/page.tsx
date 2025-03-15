db70a7c048765008c5b8947fa1f049996c3994c6
Sencoool <s6506021610037@email.kmutnb.ac.th>
2025-03-16 02:09:50 +0700

    prevent blank input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import axios from "axios"
-import { log } from "console";
-import {useEffect, useState } from "react"
+import {useEffect, useRef, useState } from "react"
 
 export default function Home() {
   const [champions, setChampions] = useState([]);
   const [search, setSearch] = useState([]);
+  const [answer, setAnswer] = useState<string[]>([]);
   const [randomChampion, setRandomChampion] = useState<any>([]);
+  const inputRef = useRef<any>(null);
 
   // Search character
   const searchCharacter = (input: string) => {
@@ -16,6 +17,20 @@ export default function Home() {
     );
     setSearch(data);
   };
+
+  const handleClick = (input: string) => {
+    let data: string[] = [];
+
+    data.push(input);
+    setAnswer(data => [...data, input]);
+
+    if(inputRef.current){
+      inputRef.current.value = "";
+      setChampions([...champions]);
+    }
+
+    setSearch([]);
+  }
   
   // Get champions
   useEffect(() => {
@@ -23,12 +38,13 @@ export default function Home() {
       const res = await axios.get(
         "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/en_US/champion.json"
       );
-      let championsArray: any = Object.values(res.data.data); // Change object to array of champions
+      let championsArray : any = Object.values(res.data.data); // Change object to array of champions
       setChampions(championsArray);
   
       const index = Math.floor(Math.random() * championsArray.length); // Randomize champion
       setRandomChampion(championsArray[index]);
     }
+
     getChampions();
   }, []);
   
@@ -36,37 +52,27 @@ export default function Home() {
   return (
     <>
       <main className="flex flex-col items-center min-h-screen py-2 container mx-auto pt-[100px]">
+        {/* search box */}
         <div className="container mx-auto items-center flex flex-col border-2 border-blue-500 p-4 rounded-lg w-1/2">
           <h1 className="text-3xl font-bold mb-10">League Champions Guessing</h1>
           <h1>Answer is : {randomChampion.name} </h1>
-          <input type="text" list="characters" name="search" id="search" className="bg-white text-black p-2 w-1/2 rounded-lg" placeholder="Type character name . . . ." onChange={e => searchCharacter(e.target.value)}/>
-          <div className="relative w-1/2 max-h-[340px] overflow-y-scroll custom-scrollbar">
+          <input type="text" list="characters" name="search" id="search" className="bg-white text-black p-2 w-1/2 rounded-lg" placeholder="Type character name . . . ." onChange={e => searchCharacter(e.target.value)} ref={inputRef}/>
+          <div className="relative w-1/2 max-h-[340px] overflow-y-scroll">
           {search.map((champion: any, index) => (
-            <div className="flex items-center gap-4 p-2 border-b bg-neutral-800 border-[#ccc] hover:bg-rose-950 duration-300 cursor-pointer" key={index}>
-              <img width={50} height={50} src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.id}.png`}  alt="" className="w-10 h-10 object-cover"/>
+            <div className="flex items-center gap-4 p-2 border-b bg-neutral-800 border-[#ccc] hover:bg-rose-950 duration-300 cursor-pointer" key={index} onClick={e => handleClick(champion.id)}>
+              <img width={50} height={50} src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.id}.png`}  alt="" className="w-10 h-10 object-cover" />
               {champion.name}
             </div>
           ))}
-      </div>
-           <div>
-            {/* {champions.length > 0 ? (
-              <div>
-                {champions.filter((champion: any) => { // filter champion name
-                  return search === "" ? champion : champion.name.toLowerCase().includes(search.toLowerCase());
-                }).map((champion: any) => (
-                  <div key={champion.id}>
-                    <img
-                      src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.id}.png`}
-                      alt={champion.name}
-                      />
-                    {champion.name}
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <p>Loading...</p>
-            )} */}
           </div>
+        </div>
+        <div className="flex flex-col items-center mt-10">
+           Already Answered
+           <div>
+            {answer.map((champions, index) => (
+              <div key={index}>{champions}</div>
+            ))}
+           </div>
         </div>
       </main>
     </>
